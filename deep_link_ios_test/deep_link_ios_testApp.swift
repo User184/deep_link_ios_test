fcac22eb32e6fa861168aca092d6d0e913f6396b
Yurii Kruchenko <yurii.kryuchenko@webpp.ru>
2025-03-25 10:55:20 -0700

    Initial Commit

---
+++ deep_link_ios_test/deep_link_ios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  deep_link_ios_testApp.swift
+//  deep_link_ios_test
+//
+//  Created by User on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct deep_link_ios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
